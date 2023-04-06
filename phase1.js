3bd72292f8ab5a80133cca08396c6d2d099a5519
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-06 12:53:33 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -30,13 +30,25 @@ app.use(express.urlencoded({ extended: true }));
 //   const token = req.body.token;
 //   await ticket.save();
 // });
-app.post('/rest/ticket', (req, res) => {
-  const ticket = req.body;
-  ticket.id = tickets.length + 1;
-  tickets.push(ticket);
-  saveTickets();
-  res.json(ticket);
+app.post('/rest/data', (req, res) => {
+  const data = req.body.data;
+  fs.writeFile('mydata.txt', data, 'utf8', err => {
+    if (err) {
+      console.error(`Failed to write data to file: ${err}`);
+      res.status(500).send('Failed to write data to file');
+    } else {
+      res.send('Data written to file');
+    }
+  });
 });
+
+// app.post('/rest/ticket', (req, res) => {
+//   const ticket = req.body;
+//   ticket.id = tickets.length + 1;
+//   tickets.push(ticket);
+//   saveTickets();
+//   res.json(ticket);
+// });
 
 
 // app.get('/rest/tickets/:id', async (req, res) => {
@@ -48,53 +60,87 @@ app.post('/rest/ticket', (req, res) => {
 //   const ticket = await Ticket.findByIdAndUpdate(req.params.id, req.body, { new: true });
 //   res.json(ticket);
 // });
-
-app.get('/rest/ticket/:id', (req, res) => {
-  const ticket = tickets.find(t => t.id == req.params.id);
-  if (!ticket) {
-    res.sendStatus(404);
-  } else {
-    res.json(ticket);
-  }
+app.get('/rest/data', (req, res) => {
+  fs.readFile('mydata.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Failed to read data from file: ${err}`);
+      res.status(500).send('Failed to read data from file');
+    } else {
+      res.send(data);
+    }
+  });
 });
 
-app.put('/rest/ticket/:id', (req, res) => {
-  const ticket = tickets.find(t => t.id == req.params.id);
-  if (!ticket) {
-    res.sendStatus(404);
-  } else {
-    ticket.created = req.body.created;
-    ticket.updated = req.body.updated;
-    ticket.type = req.body.type;
-    ticket.subject = req.body.subject;
-    ticket.description = req.body.description;
-    ticket.priority = req.body.priority;
-    ticket.status = req.body.status;
-    ticket.recipient = req.body.recipient;
-    ticket.submitter = req.body.submitter;
-    ticket.assigneeid = req.body.assigneeid;
-    ticket.followerids = req.body.followerids;
-    ticket.tags = req.body.tags;
-    saveTickets();
-    res.json(ticket);
-  }
+// app.get('/rest/ticket/:id', (req, res) => {
+//   const ticket = tickets.find(t => t.id == req.params.id);
+//   if (!ticket) {
+//     res.sendStatus(404);
+//   } else {
+//     res.json(ticket);
+//   }
+// });
+
+app.put('/rest/data/id', (req, res) => {
+  const data = req.body.data;
+  fs.writeFile('mydata.txt', data, 'utf8', err => {
+    if (err) {
+      console.error(`Failed to write data to file: ${err}`);
+      res.status(500).send('Failed to write data to file');
+    } else {
+      res.send('Data updated in file');
+    }
+  });
 });
+
+// app.put('/rest/ticket/:id', (req, res) => {
+//   const ticket = tickets.find(t => t.id == req.params.id);
+//   if (!ticket) {
+//     res.sendStatus(404);
+//   } else {
+//     ticket.created = req.body.created;
+//     ticket.updated = req.body.updated;
+//     ticket.type = req.body.type;
+//     ticket.subject = req.body.subject;
+//     ticket.description = req.body.description;
+//     ticket.priority = req.body.priority;
+//     ticket.status = req.body.status;
+//     ticket.recipient = req.body.recipient;
+//     ticket.submitter = req.body.submitter;
+//     ticket.assigneeid = req.body.assigneeid;
+//     ticket.followerids = req.body.followerids;
+//     ticket.tags = req.body.tags;
+//     saveTickets();
+//     res.json(ticket);
+//   }
+// });
 
 // app.delete('/rest/tickets/:id', async (req, res) => {
 //   await Ticket.findByIdAndDelete(req.params.id);
 //   res.sendStatus(204);
 // });
 
-app.delete('/rest/ticket/:id', (req, res) => {
-  const index = tickets.findIndex(t => t.id == req.params.id);
-  if (index === -1) {
-    res.sendStatus(404);
-  } else {
-    tickets.splice(index, 1);
-    saveTickets();
-    res.sendStatus(204);
-  }
+app.delete('/rest/data/id', (req, res) => {
+  fs.unlink('mydata.txt', err => {
+    if (err) {
+      console.error(`Failed to delete file: ${err}`);
+      res.status(500).send('Failed to delete file');
+    } else {
+      res.send('File deleted');
+    }
+  });
 });
+
+
+// app.delete('/rest/ticket/:id', (req, res) => {
+//   const index = tickets.findIndex(t => t.id == req.params.id);
+//   if (index === -1) {
+//     res.sendStatus(404);
+//   } else {
+//     tickets.splice(index, 1);
+//     saveTickets();
+//     res.sendStatus(204);
+//   }
+// });
 
 
 app.get('/', function(req, res) {
