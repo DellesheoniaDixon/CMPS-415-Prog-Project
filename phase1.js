5543f9af8f69ff09edb7b50840420edf935415fb
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-06 19:39:59 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -27,22 +27,27 @@ app.get('/rest/list', (req, res) => {
   });
 });
 
-app.get('/rest/ticket/:id', (req, res) => {
-  const id = req.params.id;
-  // Read the data from file
-  const fileData = fs.readFileSync('mydata.txt', 'utf8');
-  // Split the data into an array of lines
-  const dataLines = fileData.split('\n');
-  // Find the line with the matching ID
-  const matchingLine = dataLines.find(line => line.startsWith(id));
-  // If a matching line was found, return the data
-  if (matchingLine) {
-    const matchingData = matchingLine.split(',')[1];
-    res.send(matchingData);
-  } else {
-    res.status(404).send('Data not found');
-  }
+app.get('/rest/ticket/:id', function(req, res) {
+  const searchKey = "{ id: '" + req.params.id + "'}";
+  console.log("Looking for: " + searchKey); 
 });
+
+// app.get('/rest/ticket/:id', (req, res) => {
+//   const id = req.params.id;
+//   // Read the data from file
+//   const fileData = fs.readFileSync('mydata.txt', 'utf8');
+//   // Split the data into an array of lines
+//   const dataLines = fileData.split('\n');
+//   // Find the line with the matching ID
+//   const matchingLine = dataLines.find(line => line.startsWith(id));
+//   // If a matching line was found, return the data
+//   if (matchingLine) {
+//     const matchingData = matchingLine.split(',')[1];
+//     res.send(matchingData);
+//   } else {
+//     res.status(404).send('Data not found');
+//   }
+// });
 
 // app.post('/rest/ticket', (req, res) => {
 //   const newData = req.body.data;
@@ -58,28 +63,47 @@ app.get('/rest/ticket/:id', (req, res) => {
 
 
 // Route handler for POST /rest/data/:id
-app.post('/rest/data/:id', (req, res) => {
-  const id = req.params.id;
-  const newData = req.body.data;
+// app.post('/rest/data/:id', (req, res) => {
+//   const id = req.params.id;
+//   const newData = req.body.data;
 
-  // Read the data from file
-  const fileData = fs.readFileSync('mydata.txt', 'utf8');
-  // Split the data into an array of lines
-  const dataLines = fileData.split('\n');
-  // Find the index of the line with the matching ID
-  const matchingLineIndex = dataLines.findIndex(line => line.startsWith(id));
-  // If a matching line was found, update the data
-  if (matchingLineIndex >= 0) {
-    const matchingData = dataLines[matchingLineIndex].split(',')[1];
-    const updatedLine = `${id},${newData}`;
-    dataLines[matchingLineIndex] = updatedLine;
-    // Write the updated data back to file
-    fs.writeFileSync('mydata.txt', dataLines.join('\n'));
-    res.send(`Data associated with ID ${id} updated to ${newData}`);
-  } else {
-    res.status(404).send('Data not found');
+//   // Read the data from file
+//   const fileData = fs.readFileSync('mydata.txt', 'utf8');
+//   // Split the data into an array of lines
+//   const dataLines = fileData.split('\n');
+//   // Find the index of the line with the matching ID
+//   const matchingLineIndex = dataLines.findIndex(line => line.startsWith(id));
+//   // If a matching line was found, update the data
+//   if (matchingLineIndex >= 0) {
+//     const matchingData = dataLines[matchingLineIndex].split(',')[1];
+//     const updatedLine = `${id},${newData}`;
+//     dataLines[matchingLineIndex] = updatedLine;
+//     // Write the updated data back to file
+//     fs.writeFileSync('mydata.txt', dataLines.join('\n'));
+//     res.send(`Data associated with ID ${id} updated to ${newData}`);
+//   } else {
+//     res.status(404).send('Data not found');
+//   }
+// });
+
+app.post('/rest/ticket/', function(req, res) {
+  const ticket = {
+    id: req.body.id,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at,
+    type: req.body.type,
+    subject: req.body.subject,
+    description: req.body.description,
+    priority: req.body.priority,
+    status: req.body.status,
+    recipient: req.body.recipient,
+    submitter: req.body.submitter,
+    assignee_id: req.body.assignee_id,
+    follower_ids: req.body.follower_ids,
+    tags: req.body.tags,
   }
-});
+
+  const jsonString = JSON.stringify(ticket);
 
 app.get('/', function(req, res) {
   const myquery = req.query;
