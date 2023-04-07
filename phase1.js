d4d05c2d3eda2c18631cfda246115d737a1cd207
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-06 20:36:32 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -9,6 +9,8 @@ console.log('Server started at http://localhost:' + port);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Define routes
 //rest makes it more specific to the 'ticket' resourse
@@ -44,71 +46,8 @@ app.get('/rest/ticket/:id', (req, res) => {
   }
 });
 
-// app.post('/rest/ticket', (req, res) => {
-//   const newData = req.body.data;
-//   // Read the existing data from file
-//   const fileData = fs.readFileSync('mydata.txt', 'utf8');
-//   // Append the new data to the existing data
-//   const updatedData = fileData + '\n' + newData;
-//   // Write the updated data back to the file
-//   fs.writeFileSync('mydata.txt', updatedData);
-//   // Send a response to the client
-//   res.send('Data updated successfully');
-// });
 
 
-// Route handler for POST /rest/data/:id
-// app.post('/rest/data/:id', (req, res) => {
-//   const id = req.params.id;
-//   const newData = req.body.data;
-
-//   // Read the data from file
-//   const fileData = fs.readFileSync('mydata.txt', 'utf8');
-//   // Split the data into an array of lines
-//   const dataLines = fileData.split('\n');
-//   // Find the index of the line with the matching ID
-//   const matchingLineIndex = dataLines.findIndex(line => line.startsWith(id));
-//   // If a matching line was found, update the data
-//   if (matchingLineIndex >= 0) {
-//     const matchingData = dataLines[matchingLineIndex].split(',')[1];
-//     const updatedLine = `${id},${newData}`;
-//     dataLines[matchingLineIndex] = updatedLine;
-//     // Write the updated data back to file
-//     fs.writeFileSync('mydata.txt', dataLines.join('\n'));
-//     res.send(`Data associated with ID ${id} updated to ${newData}`);
-//   } else {
-//     res.status(404).send('Data not found');
-//   }
-// });
-
-// app.post('/rest/ticket', (req, res) => {
-//   // Read the current data from the file
-//   fs.readFile('mydata.txt', 'utf8', (err, data) => {
-//     if (err) {
-//       console.error(err);
-//       res.status(500).send('Error reading file');
-//       return;
-//     }
-
-//     // Parse the data into an array of ticket objects
-//     let tickets = JSON.parse(data);
-
-//     // Add the new ticket object to the array
-//     const newTicket = req.body;
-//     tickets.push(newTicket);
-
-//     // Write the updated data back to the file
-//     fs.writeFile('mydata.txt', JSON.stringify(tickets), (err) => {
-//       if (err) {
-//         console.error(err);
-//         res.status(500).send('Error writing file');
-//         return;
-//       }
-
-//       res.send(`Ticket ${newTicket.id} created`);
-//     });
-//   });
-// });
 app.post('/rest/ticket', (req, res) => {
   // Read the existing data from the file
   const fileData = fs.readFileSync('mydata.txt');
@@ -150,34 +89,4 @@ app.get('/', function(req, res) {
   res.send(ticket);
 });
 
-
-// Write to a file 
-
-// app.get('/wfile', function(req, res) {
-//   const myquery = req.query;
-  
-//   var ticket = '';
-//   for(var key in myquery) { ticket += "--" + key + ">" + myquery[key]; }
-//   fs.appendFile("mydata.txt", ticket+'\n', (err) => {
-//     if (err)
-//       console.log(err);
-//     else {
-//       console.log("File written successfully\n");
-//       console.log("Contents of file now:\n");
-//       console.log(fs.readFileSync("mydata.txt", "utf8"));
-//     }
-//   });
- 
-//   res.send(ticket);
-
-// });
-
-
-// Simple cascade
-// app.param('name', function(req, res, next, name) {
-//   const modified = name.toUpperCase();
-
-//   req.name = modified;
-//   next();
-// });
 
