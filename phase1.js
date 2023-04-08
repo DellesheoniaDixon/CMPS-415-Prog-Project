05a5f5bdac08d9b41316a6f37bbed1807ca1722a
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-07 21:24:12 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -36,54 +36,84 @@ app.get('/rest/ticket/:id', function(req, res) {
 
 
 
+app.post('/post/ticket', function(req, res) {
+  const id = req.body.id;
+  const created_at = new Date().toISOString();
+  const updated_at = new Date().toISOString();
+  const type = req.body.type;
+  const subject = req.body.subject;
+  const description = req.body.description;
+  const priority = req.body.priority;
+  const status = req.body.status;
+  const recipient = req.body.recipient;
+  const submitter = req.body.submitter;
+  const assignee_id = req.body.assignee_id;
+  const follower_ids = req.body.follower_ids || [];
+  const tags = req.body.tags || [];
 
-
-app.post('/rest/ticket', (req, res) => {
-  // Generate new ID for ticket
-  const newId = Date.now().toString();
-  
-  // Construct new ticket object
-  const newTicket = {
-    id: newId,
-    created_at: new Date().toISOString(),
-    updated_at: new Date().toISOString(),
-    type: req.body.type,
-    subject: req.body.subject,
-    description: req.body.description,
-    priority: req.body.priority,
-    status: req.body.status,
-    recipient: req.body.recipient,
-    submitter: req.body.submitter,
-    assignee_id: req.body.assignee_id,
-    follower_ids: req.body.follower_ids || [],
-    tags: req.body.tags || []
-  };
-  
-  // Read current ticket data
-  fs.readFile('mydata.txt', 'utf8', (err, data) => {
-    if (err) {
-      console.error(`Failed to read data from file: ${err}`);
-      res.status(500).send('Failed to read data from file');
-    } else {
-      let tickets = [];
-      if (data) {
-        tickets = JSON.parse(data);
-      }
-      // Add new ticket to list of tickets
-      tickets.push(newTicket);
-      
-      // Write updated ticket data to file
-      fs.writeFile('mydata.txt', JSON.stringify(tickets), 'utf8', err => {
-        if (err) {
-          console.error(`Failed to write data to file: ${err}`);
-          res.status(500).send('Failed to write data to file');
-        } else {
-          res.send(newTicket);
-        }
-      });
-    }
+  res.send({
+    'id': id,
+    'created_at': created_at,
+    'updated_at': updated_at,
+    'type': type,
+    'subject': subject,
+    'description': description,
+    'priority': priority,
+    'status': status,
+    'recipient': recipient,
+    'submitter': submitter,
+    'assignee_id': assignee_id,
+    'follower_ids': follower_ids,
+    'tags': tags
   });
 });
+
+// app.post('/rest/ticket', (req, res) => {
+//   // Generate new ID for ticket
+//   const newId = Date.now().toString();
+  
+//   // Construct new ticket object
+//   const newTicket = {
+//     id: newId,
+//     created_at: new Date().toISOString(),
+//     updated_at: new Date().toISOString(),
+//     type: req.body.type,
+//     subject: req.body.subject,
+//     description: req.body.description,
+//     priority: req.body.priority,
+//     status: req.body.status,
+//     recipient: req.body.recipient,
+//     submitter: req.body.submitter,
+//     assignee_id: req.body.assignee_id,
+//     follower_ids: req.body.follower_ids || [],
+//     tags: req.body.tags || []
+//   };
+  
+//   // Read current ticket data
+//   fs.readFile('mydata.txt', 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(`Failed to read data from file: ${err}`);
+//       res.status(500).send('Failed to read data from file');
+//     } else {
+//       let tickets = [];
+//       if (data) {
+//         tickets = JSON.parse(data);
+//       }
+//       // Add new ticket to list of tickets
+//       tickets.push(newTicket);
+      
+//       // Write updated ticket data to file
+//       fs.writeFile('mydata.txt', JSON.stringify(tickets), 'utf8', err => {
+//         if (err) {
+//           console.error(`Failed to write data to file: ${err}`);
+//           res.status(500).send('Failed to write data to file');
+//         } else {
+//           res.send(newTicket);
+//         }
+//       });
+//     }
+//   });
+// });
 
 
 
