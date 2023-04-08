d8307b96f18db602c837dab2b2ec24c2c9e86ea4
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-07 22:01:11 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -40,14 +40,53 @@ app.get('/rest/ticket/:id', (req, res) => {
 });
 
 
-app.post('/rest/ticket', (req, res) => {
-  const { title, description } = req.body;
-  if (!title || !description) {
-    res.status(400).json({ error: 'Unable to access ticket information' });
-  } else {
-    const newTicket = tickets.addTicket(title, description);
-    res.json(newTicket);
+
+class Ticket {
+  constructor(id, type, subject, description, priority, status, recipient, submitter, assignee_id, follower_ids, tags) {
+    this.id = id;
+    this.type = type;
+    this.subject = subject;
+    this.description = description;
+    this.priority = priority;
+    this.status = status;
+    this.recipient = recipient;
+    this.submitter = submitter;
+    this.assignee_id = assignee_id;
+    this.follower_ids = follower_ids;
+    this.tags = tags;
+    this.created_at = new Date().toISOString();
+    this.updated_at = new Date().toISOString();
   }
+}
+
+// In-memory array to store tickets (for demo purposes)
+let tickets = [];
+
+// Endpoint to create a new ticket
+app.post('/rest/ticket', (req, res) => {
+  // Generate a new ID for the ticket
+  const newId = tickets.length + 1;
+
+  // Create a new ticket object from the request body
+  const newTicket = new Ticket(
+    newId,
+    req.body.type,
+    req.body.subject,
+    req.body.description,
+    req.body.priority,
+    req.body.status,
+    req.body.recipient,
+    req.body.submitter,
+    req.body.assignee_id,
+    req.body.follower_ids || [],
+    req.body.tags || []
+  );
+
+  // Add the new ticket to the array
+  tickets.push(newTicket);
+
+  // Return the new ticket object as JSON
+  res.json(newTicket);
 });
 
 
