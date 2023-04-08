16c94d78cc0a0fec9a9f85d0f69b37614a5f9926
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-07 19:44:43 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -3,6 +3,7 @@ const bodyParser=require('body-parser');
 const app = express();
 const port = 3000;
 var fs = require("fs");
+const request = require('request');
 
 app.listen(port);
 console.log('Server started at http://localhost:' + port);
@@ -46,49 +47,55 @@ app.get('/rest/ticket/:id', (req, res) => {
   }
 });
 
+const data = fs.readFileSync('mydata.txt', 'utf8');
 
-
-app.post('/rest/ticket', (req, res) => {
-  // Read the existing data from the file
-//   const fileData = fs.readFileSync('mydata.txt');
-//   const data = JSON.parse(fileData);
-  
-  // Generate a new ID for the ticket
-  const newId = data.length > 0 ? data[data.length - 1].id + 1 : 1;
-  
-  // Create a new ticket object with the request data and generated ID
-  const newTicket = {
-    id: newId,
-    created_at: new Date().toISOString(),
-    updated_at: new Date().toISOString(),
-    type: req.body.type,
-    subject: req.body.subject,
-    description: req.body.description,
-    priority: req.body.priority,
-    status: req.body.status,
-    recipient: req.body.recipient,
-    submitter: req.body.submitter,
-    assignee_id: req.body.assignee_id,
-    follower_ids: req.body.follower_ids || [],
-    tags: req.body.tags || []
-  };
-  
-  // Add the new ticket to the data array
-  data.push(newTicket);
-  
-  // Write the updated data back to the file
-  fs.writeFileSync('mydata.txt', JSON.stringify(data, null, 2));
-  
-  // Send the new ticket object in the response
-  res.json(newTicket);
+request.post('https://cmps415project.onrender.com/rest/ticket', {
+  json: { data }
+}, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log(body);
+  }
 });
+
+
+// app.post('/rest/ticket', (req, res) => {
+//   // Read the existing data from the file
+// //   const fileData = fs.readFileSync('mydata.txt');
+// //   const data = JSON.parse(fileData);
+  
+//   // Generate a new ID for the ticket
+//   const newId = data.length > 0 ? data[data.length - 1].id + 1 : 1;
+  
+//   // Create a new ticket object with the request data and generated ID
+//   const newTicket = {
+//     id: newId,
+//     created_at: new Date().toISOString(),
+//     updated_at: new Date().toISOString(),
+//     type: req.body.type,
+//     subject: req.body.subject,
+//     description: req.body.description,
+//     priority: req.body.priority,
+//     status: req.body.status,
+//     recipient: req.body.recipient,
+//     submitter: req.body.submitter,
+//     assignee_id: req.body.assignee_id,
+//     follower_ids: req.body.follower_ids || [],
+//     tags: req.body.tags || []
+//   };
+  
+//   // Add the new ticket to the data array
+//   data.push(newTicket);
+  
+//   // Write the updated data back to the file
+//   fs.writeFileSync('mydata.txt', JSON.stringify(data, null, 2));
+  
+//   // Send the new ticket object in the response
+//   res.json(newTicket);
+// });
 
 app.get('/', function(req, res) {
-  const myquery = req.query;
-  var ticket = 'Starting... ';
-  res.send(ticket);
-});
-app.post('/', function(req, res) {
   const myquery = req.query;
   var ticket = 'Starting... ';
   res.send(ticket);
