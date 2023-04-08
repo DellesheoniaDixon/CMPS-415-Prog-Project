54764c033f8c68b85337489c856190cb9f8020c7
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-08 15:58:10 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -59,97 +59,74 @@ app.get('/rest/ticket/:id', function(req,res){
   });
 
 
-// // Define the Ticket class
-// class Ticket {
-//   constructor(
-//     id,
-//     type,
-//     subject,
-//     description,
-//     priority,
-//     status,
-//     recipient,
-//     submitter,
-//     assignee_id,
-//     follower_ids,
-//     tags
-//   ) {
-//     this.id = id;
-//     this.type = type;
-//     this.subject = subject;
-//     this.description = description;
-//     this.priority = priority;
-//     this.status = status;
-//     this.recipient = recipient;
-//     this.submitter = submitter;
-//     this.assignee_id = assignee_id;
-//     this.follower_ids = follower_ids;
-//     this.tags = tags;
-//     this.created_at = new Date().toISOString();
-//     this.updated_at = new Date().toISOString();
-//   }
-// }
-
-// // In-memory array to store tickets
-// let tickets = [];
-
-// // Endpoint to create a new ticket
-// app.post('/rest/ticket', (req, res) => {
-//   // Generate a new ID for the ticket
-//   const newId = tickets.length + 1;
-
-//   // Create a new ticket object from the request body
-//   const newTicket = new Ticket(
-//     newId,
-//     req.body.type,
-//     req.body.subject,
-//     req.body.description,
-//     req.body.priority,
-//     req.body.status,
-//     req.body.recipient,
-//     req.body.submitter,
-//     req.body.assignee_id,
-//     req.body.follower_ids || [],
-//     req.body.tags || []
-//   );
-
-//   // Add the new ticket to the array
-//   tickets.push(newTicket);
-
-//   // Return the new ticket object as JSON
-//   res.json(newTicket);
-// });
 
 
+// In-memory array to store tickets (for demo purposes)
+let tickets = [];
 
-app.post('/rest/ticket/', function(req,res){
-   const data = req.body;
-   fs.readFile("./mydata.json", 'utf8', (err,jsonString) => {
-    if (err){
-        console.error(err);
-      return;
+// Read data from file on server start-up
+fs.readFile('mydata.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(`Failed to read data from file: ${err}`);
+  } else {
+    tickets = JSON.parse(data);
+    console.log(`Loaded ${tickets.length} tickets from file.`);
+  }
+});
+
+class Ticket {
+  constructor(id, type, subject, description, priority, status, recipient, submitter, assignee_id, follower_ids, tags) {
+    this.id = id;
+    this.type = type;
+    this.subject = subject;
+    this.description = description;
+    this.priority = priority;
+    this.status = status;
+    this.recipient = recipient;
+    this.submitter = submitter;
+    this.assignee_id = assignee_id;
+    this.follower_ids = follower_ids;
+    this.tags = tags;
+    this.created_at = new Date().toISOString();
+    this.updated_at = new Date().toISOString();
+  }
+}
+
+// Endpoint to create a new ticket and write data to file
+app.post('/rest/ticket', (req, res) => {
+  // Generate a new ID for the ticket
+  const newId = tickets.length + 1;
+
+  // Create a new ticket object from the request body
+  const newTicket = new Ticket(
+    newId,
+    req.body.type,
+    req.body.subject,
+    req.body.description,
+    req.body.priority,
+    req.body.status,
+    req.body.recipient,
+    req.body.submitter,
+    req.body.assignee_id,
+    req.body.follower_ids || [],
+    req.body.tags || []
+  );
+
+  // Add the new ticket to the array
+  tickets.push(newTicket);
+
+  // Write the updated `tickets` array to file
+  fs.writeFile('mydata.json', JSON.stringify(tickets), (err) => {
+    if (err) {
+      console.error(`Failed to write data to file: ${err}`);
+      res.status(500).send('Failed to write data to file');
+    } else {
+      res.json(newTicket);
     }
-    try{
-      const currentdata = JSON.parse(jsonString);
-      currentdata.push(data);
-    
-    fs.writeFile("./mydata.json", JSON.stringify(currentdata, null, 2), (err) => {
-        if (err){
-            console.error(err);
-            res.status(500);
-        }
-        else{
-            console.log("File written successfully.");
-            res.status(200).json(data);
-        }
-    });
-  }
-  catch(err){
-    console.error(err);
-    res.status(500).send("Error parsing JSON data!");
-  }
+  });
 });
-});
+
+
 
 
 
