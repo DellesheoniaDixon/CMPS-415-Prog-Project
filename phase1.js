b44c556d9853eeb84caa82ab3d82eb2249882e7c
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-08 12:44:38 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -32,46 +32,45 @@ app.get('/rest/list', (req, res) => {
 
 
 
-// app.get('/rest/ticket/:id', (req, res) => {
-//   const ticket = tickets.getTicketById(req.params.id);
-//   if (ticket) {
-//     res.json(ticket);
-//   } else {
-//     res.status(404).json({ error: 'Ticket not found' });
-//   }
-// });
-
+app.get('/rest/ticket/:id', (req, res) => {
+  const ticket = tickets.getTicketById(req.params.id);
+  if (ticket) {
+    res.json(ticket);
+  } else {
+    res.status(404).json({ error: 'Ticket not found' });
+  }
+});
 
 
 // Define the Ticket class
 class Ticket {
-constructor(
-id,
-type,
-subject,
-description,
-priority,
-status,
-recipient,
-submitter,
-assignee_id,
-follower_ids,
-tags
-) {
-this.id = id;
-this.type = type;
-this.subject = subject;
-this.description = description;
-this.priority = priority;
-this.status = status;
-this.recipient = recipient;
-this.submitter = submitter;
-this.assignee_id = assignee_id;
-this.follower_ids = follower_ids;
-this.tags = tags;
-this.created_at = new Date().toISOString();
-this.updated_at = new Date().toISOString();
-}
+  constructor(
+    id,
+    type,
+    subject,
+    description,
+    priority,
+    status,
+    recipient,
+    submitter,
+    assignee_id,
+    follower_ids,
+    tags
+  ) {
+    this.id = id;
+    this.type = type;
+    this.subject = subject;
+    this.description = description;
+    this.priority = priority;
+    this.status = status;
+    this.recipient = recipient;
+    this.submitter = submitter;
+    this.assignee_id = assignee_id;
+    this.follower_ids = follower_ids;
+    this.tags = tags;
+    this.created_at = new Date().toISOString();
+    this.updated_at = new Date().toISOString();
+  }
 }
 
 // In-memory array to store tickets
@@ -79,44 +78,31 @@ let tickets = [];
 
 // Endpoint to create a new ticket
 app.post('/rest/ticket', (req, res) => {
-// Generate a new ID for the ticket
-const newId = tickets.length + 1;
+  // Generate a new ID for the ticket
+  const newId = tickets.length + 1;
 
-// Create a new ticket object from the request body
-const newTicket = new Ticket(
-newId,
-req.body.type,
-req.body.subject,
-req.body.description,
-req.body.priority,
-req.body.status,
-req.body.recipient,
-req.body.submitter,
-req.body.assignee_id,
-req.body.follower_ids || [],
-req.body.tags || []
-);
+  // Create a new ticket object from the request body
+  const newTicket = new Ticket(
+    newId,
+    req.body.type,
+    req.body.subject,
+    req.body.description,
+    req.body.priority,
+    req.body.status,
+    req.body.recipient,
+    req.body.submitter,
+    req.body.assignee_id,
+    req.body.follower_ids || [],
+    req.body.tags || []
+  );
 
-// Add the new ticket to the array
-tickets.push(newTicket);
+  // Add the new ticket to the array
+  tickets.push(newTicket);
 
-// Return the new ticket object as JSON
-res.json(newTicket);
+  // Return the new ticket object as JSON
+  res.json(newTicket);
 });
 
-// Endpoint to get a ticket by ID
-app.get('/rest/ticket/:id', (req, res) => {
-// Find the ticket with the given ID in the array
-const ticket = tickets.find((x) => x.id === Number(req.params.id));
-
-// If the ticket is found, return it as JSON
-if (ticket) {
-res.status(200).send(JSON.stringify(ticket, null, 2));
-} else {
-// Otherwise, return a 404 error with a message
-res.status(404).send(ticket with id ${req.params.id} not found.);
-}
-});
 app.get('/', function(req, res) {
   const myquery = req.query;
   var ticket = 'Starting... ';
