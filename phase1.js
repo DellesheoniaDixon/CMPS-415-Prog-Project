b168f9f4e1297bdf095ef2ca073ad329d0b23888
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-08 23:02:05 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -1,135 +1,52 @@
 const express = require('express');
 const fs = require('fs');
-const path = require('path');
 
-const filePath = './mydata.json';
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-class TicketsController {
-  constructor() {
-    this.router = express.Router();
-    this.router.get('/rest/list', this.getList.bind(this));
-    this.router.get('/rest/ticket/:id', this.getTicket.bind(this));
-    this.router.post('/rest/ticket', this.createTicket.bind(this));
+let tickets = [];
+
+// Load initial tickets from mydata.json
+fs.readFile('mydata.json', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    tickets = JSON.parse(data);
   }
+});
 
-  getTickets() {
-    const rawData = fs.readFileSync(path.resolve(__dirname, filePath));
-    const jsonData = JSON.parse(rawData.toString());
-    return jsonData.map((data) => ({
-      id: data.id,
-      created_at: data.created_at,
-      updated_at: data.updated_at,
-      type: data.type,
-      subject: data.subject,
-      description: data.description,
-      priority: data.priority,
-      status: data.status,
-      recipient: data.recipient,
-      submitter: data.submitter,
-      assignee_id: data.assignee_id,
-      follower_ids: data.follower_ids,
-      tags: data.tags,
-    }));
+// Endpoint to get all tickets
+app.get('/rest/list', (req, res) => {
+  res.send(tickets);
+});
+
+// Endpoint to get a single ticket by id
+app.get('/rest/ticket/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const ticket = tickets.find((t) => t.id === id);
+
+  if (!ticket) {
+    res.status(404).send('Ticket not found');
+  } else {
+    res.send(ticket);
   }
+});
 
-  getNextId(tickets) {
-    return tickets.reduce((maxId, ticket) => {
-      return Math.max(maxId, ticket.id) + 1;
-    }, 1);
-  }
+// Endpoint to create a new ticket
+app.post('/rest/ticket', express.json(), (req, res) => {
+  const ticket = req.body;
+  ticket.id = Date.now(); // Assign a unique id
+  tickets.push(ticket);
 
-  validateTicket(ticket) {
-    const requiredProperties = [
-      'type',
-      'subject',
-      'description',
-      'priority',
-      'status',
-      'recipient',
-      'submitter',
-      'assignee_id',
-    ];
+  res.send(ticket);
+});
 
-    const errorMessages = requiredProperties
-      .filter((property) => !ticket[property])
-      .map(this.getRequiredText);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
 
-    return {
-      hasErrors: errorMessages.length > 0,
-      errorMessages,
-    };
-  }
-
-  getRequiredText(property) {
-    return `${property} is required`;
-  }
-
-  async writeTicket(ticket) {
-    const tickets = this.getTickets();
-
-    const nextTicketId = this.getNextId(tickets);
-    ticket.id = nextTicketId;
-    ticket.created_at = new Date().toISOString();
-    ticket.updated_at = new Date().toISOString();
-
-    const validationResult = this.validateTicket(ticket);
-
-    if (validationResult.hasErrors) {
-      return Promise.resolve({
-        data: '',
-        validationResult: validationResult,
-      });
-    }
-
-    tickets.push(ticket);
-    const updatedContent = JSON.stringify(tickets, null, 2);
-
-    try {
-      fs.writeFileSync(path.resolve(__dirname, filePath), updatedContent);
-      return Promise.resolve({
-        data: JSON.stringify(ticket, null, 2),
-      });
-    } catch (err) {
-      console.log(err);
-      return Promise.resolve({
-        data: '',
-        validationResult: {
-          hasErrors: true,
-          errorMessages: ['Failed to write ticket to file'],
-        },
-      });
-    }
-  }
-
-  async getList(req, res) {
-    const tickets = this.getTickets();
-    return res.status(200).send(JSON.stringify(tickets, null, 2));
-  }
-
-  async createTicket(req, res) {
-    const response = await this.writeTicket(req.body);
-
-    if (response.validationResult?.hasErrors) {
-      return res
-        .status(400)
-        .send(JSON.stringify(response.validationResult.errorMessages, null, 2));
-    }
-
-    return res.status(201).send(response.data);
-  }
-
-  async getTicket(req, res) {
-    const tickets = this.getTickets();
-
-    const ticket = tickets.find(function (x) {
-      return x.id === Number(req.params.id);
-    });
-
-    return ticket
-      ? res.status(200).send(JSON.stringify(ticket, null, 2))
-      : res.status
-
-// router.get('/', function(req, res) {
+// app.get('/', function(req, res) {
 //   const myquery = req.query;
 //   var ticket = 'Starting... ';
 //   res.send(ticket);
