d01a9486eb88a1f2d30687a3718c9e8b4b42e5ea
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-13 11:13:36 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -1,101 +1,81 @@
 const express = require('express');
-const fs = require('fs/promises');
+const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 const FILE_PATH = './mydata.json';
 
-// Middleware to parse JSON data
-app.use(express.json());
+// Read the initial data from file
+let tickets = JSON.parse(fs.readFileSync(FILE_PATH));
 
-// GET all items
-app.get('/rest/tickets', async (req, res) => {
-  try {
-    const data = await fs.readFile(FILE_PATH, 'utf-8');
-    res.send(JSON.parse(data));
-  } catch (err) {
-    res.status(500).send('Internal server error');
+// Endpoint to get all tickets
+app.get('/rest/list', (req, res) => {
+  res.send(tickets);
+});
+
+// Endpoint to get a single ticket by id
+app.get('/rest/ticket/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const ticket = tickets.find((t) => t.id === id);
+
+  if (!ticket) {
+    res.status(404).send('Ticket not found');
+  } else {
+    res.send(ticket);
   }
 });
 
-// GET item by ID
-app.get('/rest/tickets/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const data = await fs.readFile(FILE_PATH, 'utf-8');
-    const items = JSON.parse(data);
-    const item = items.find((i) => i.id === id);
+// Endpoint to create a new ticket
+app.post('/rest/ticket', express.json(), (req, res) => {
+  const ticket = req.body;
+  ticket.id = Date.now(); // Assign a unique id
+  tickets.push(ticket);
+  console.log(`Created ticket with id ${ticket.id}`);
 
-    if (!item) {
-      res.status(404).send('Item not found');
-    } else {
-      res.send(item);
-    }
-  } catch (err) {
-    res.status(500).send('Internal server error');
+  // Write the updated data back to the file
+  fs.writeFileSync(FILE_PATH, JSON.stringify(tickets));
+
+  res.send(ticket);
+});
+
+// Endpoint to update an existing ticket by id
+app.put('/rest/ticket/:id', express.json(), (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tickets.findIndex((t) => t.id === id);
+
+  if (index === -1) {
+    res.status(404).send('Ticket not found');
+  } else {
+    tickets[index] = req.body;
+
+    // Write the updated data back to the file
+    fs.writeFileSync(FILE_PATH, JSON.stringify(tickets));
+
+    res.send(tickets[index]);
   }
 });
 
-// POST a new item
-app.post('/rest/tickets', async (req, res) => {
-  try {
-    const data = await fs.readFile(FILE_PATH, 'utf-8');
-    const items = JSON.parse(data);
-    const newItem = req.body;
-    newItem.id = items.length + 1;
-    items.push(newItem);
-    await fs.writeFile(FILE_PATH, JSON.stringify(items));
-    res.send(newItem);
-  } catch (err) {
-    res.status(500).send('Internal server error');
+// Endpoint to delete a ticket by id
+app.delete('/rest/ticket/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tickets.findIndex((t) => t.id === id);
+
+  if (index === -1) {
+    res.status(404).send('Ticket not found');
+  } else {
+    const deletedTicket = tickets.splice(index, 1)[0];
+
+    // Write the updated data back to the file
+    fs.writeFileSync(FILE_PATH, JSON.stringify(tickets));
+
+    res.send(deletedTicket);
   }
 });
 
-// PUT (update) an existing item by ID
-app.put('/rest/tickets/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const data = await fs.readFile(FILE_PATH, 'utf-8');
-    const items = JSON.parse(data);
-    const index = items.findIndex((i) => i.id === id);
-
-    if (index === -1) {
-      res.status(404).send('Item not found');
-    } else {
-      const updatedItem = req.body;
-      updatedItem.id = id;
-      items[index] = updatedItem;
-      await fs.writeFile(FILE_PATH, JSON.stringify(items));
-      res.send(updatedItem);
-    }
-  } catch (err) {
-    res.status(500).send('Internal server error');
-  }
-});
-
-// DELETE an item by ID
-app.delete('/rest/tickets/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id);
-    const data = await fs.readFile(FILE_PATH, 'utf-8');
-    const items = JSON.parse(data);
-    const index = items.findIndex((i) => i.id === id);
-
-    if (index === -1) {
-      res.status(404).send('Item not found');
-    } else {
-      const deletedItem = items.splice(index, 1)[0];
-      await fs.writeFile(FILE_PATH, JSON.stringify(items));
-      res.send(deletedItem);
-    }
-  } catch (err) {
-    res.status(500).send('Internal server error');
-  }
-});
-
-app.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}`);
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
 });
 
 
