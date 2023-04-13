2aec45531777ab22b26bed7d1e6820bffb9f487f
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-13 18:50:52 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -54,11 +54,39 @@ app.post('/rest/ticket', express.json(), (req, res) => {
   res.send(ticket);
 });
 
+// Endpoint to update a ticket by id
+app.put('/rest/ticket/:id', express.json(), (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tickets.findIndex((t) => t.id === id);
+
+  if (index === -1) {
+    res.status(404).send('Ticket not found');
+  } else {
+    tickets[index] = req.body;
+    tickets[index].id = id;
+    console.log(`Updated ticket with id ${id}`);
+    res.send(tickets[index]);
+  }
+});
+
+// Endpoint to delete a ticket by id
+app.delete('/rest/ticket/:id', (req, res) => {
+  const id = parseInt(req.params.id);
+  const index = tickets.findIndex((t) => t.id === id);
+
+  if (index === -1) {
+    res.status(404).send('Ticket not found');
+  } else {
+    tickets.splice(index, 1);
+    console.log(`Deleted ticket with id ${id}`);
+    res.sendStatus(204);
+  }
+});
+
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
-
 
 
 // app.get('/', function(req, res) {
