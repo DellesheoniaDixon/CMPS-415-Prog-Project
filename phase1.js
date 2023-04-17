58830396f715e858ff331e70ebde10180325642d
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-17 14:32:23 -0500

    Update phase1.js

--- phase1.js
+++ phase1.js
@@ -4,16 +4,29 @@ const fs = require('fs');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-let tickets = [];
-
-// Read data from the file synchronously
-try {
-  const data = fs.readFileSync('mydata.json', 'utf8');
-  tickets = JSON.parse(data);
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
+let tickets = [
+  {
+    "id": 35436,
+    "created_at": "2015-07-20T22:55:29Z",
+    "updated_at": "2016-05-05T10:38:52Z",
+    "type": "incident",
+    "subject": "MFP not working right",
+    "description": "PC Load Letter? What does that even mean???",
+    "priority": "med",
+    "status": "open",
+    "recipient": "support_example@selu.edu",
+    "submitter": "Michael_bolton@selu.edu",
+    "assignee_id": 235323,
+    "follower_ids": [
+        235323,
+        234
+    ],
+    "tags": [
+        "enterprise",
+        "printers"
+    ]
+  }
+];
 
 // Endpoint to get all tickets
 app.get('/rest/list', (req, res) => {
@@ -41,45 +54,17 @@ app.post('/rest/ticket', express.json(), (req, res) => {
   res.send(ticket);
 });
 
-// Endpoint to update a ticket by id
-app.put('/rest/ticket/:id', express.json(), (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = tickets.findIndex((t) => t.id === id);
-
-  if (index === -1) {
-    res.status(404).send('Ticket not found');
-  } else {
-    tickets[index] = req.body;
-    tickets[index].id = id;
-    console.log(`Updated ticket with id ${id}`);
-    res.send(tickets[index]);
-  }
-});
-
-// Endpoint to delete a ticket by id
-app.delete('/rest/ticket/:id', (req, res) => {
-  const id = parseInt(req.params.id);
-  const index = tickets.findIndex((t) => t.id === id);
-
-  if (index === -1) {
-    res.status(404).send('Ticket not found');
-  } else {
-    tickets.splice(index, 1);
-    console.log(`Deleted ticket with id ${id}`);
-    res.sendStatus(204);
-  }
-});
-
 // Start the server
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
 
-// app.get('/', function(req, res) {
-//   const myquery = req.query;
-//   var ticket = 'Starting... ';
-//   res.send(ticket);
-// });
+
+app.get('/', function(req, res) {
+  const myquery = req.query;
+  var ticket = 'Starting... ';
+  res.send(ticket);
+});
 
 
 
