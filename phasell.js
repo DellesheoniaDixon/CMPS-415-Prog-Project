16c86d0af79347157aceb7f3a58e8f5d57d321c9
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-18 18:37:51 -0500

    Update phasell.js

--- phasell.js
+++ phasell.js
@@ -4,96 +4,88 @@ const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const uri = 'mongodb+srv://dellesheoniadixon2:k9UyT796OyDNGNlB@ddixonmdb.05vfedo.mongodb.net/?retryWrites=true&w=majority'; // Replace with your MongoDB URI
 
-app.use(bodyParser.json());
+// Connection URL and database name
+const uri = 'mongodb+srv://dellesheoniadixon2:k9UyT796OyDNGNlB@ddixonmdb.05vfedo.mongodb.net/?retryWrites=true&w=majority';
+const client = new MongoClient(uri, { useNewUrlParser: true });
 
 // Connect to MongoDB
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(async (err) => {
-  if (err) {
-    console.error('Failed to connect to MongoDB:', err);
-    process.exit(1);
+async function connect() {
+  try {
+    await client.connect();
+    console.log('Connected to MongoDB');
+  } catch (err) {
+    console.error(err);
   }
+}
+connect();
 
-  console.log('Connected to MongoDB');
+// Middleware to parse request body as JSON
+app.use(bodyParser.json());
 
-  // Get database and collection
-  const db = client.db('Phase-ll'); // Replace with your database name
-  const ticketsCollection = db.collection('CMPS415'); // Replace with your collection name
-
-  // Endpoint to get all tickets
-  app.get('/rest/list', async (req, res) => {
-    try {
-      const result = await ticketsCollection.find().toArray();
-      res.send(result);
-    } catch (err) {
-      console.error('Failed to get tickets from MongoDB:', err);
-      res.status(500).send('Failed to get tickets');
-    }
-  });
-
-  // Endpoint to get a single ticket by id
-  app.get('/rest/ticket/:id', async (req, res) => {
-    try {
-      const id = parseInt(req.params.id);
-      const ticket = await ticketsCollection.findOne({ id });
-
-      if (!ticket) {
-        res.status(404).send('Ticket not found');
-      } else {
-        res.send(ticket);
-      }
-    } catch (err) {
-      console.error('Failed to get ticket from MongoDB:', err);
-      res.status(500).send('Failed to get ticket');
-    }
-  });
-
-  // Endpoint to create a new ticket
-  app.post('/rest/ticket', async (req, res) => {
-    try {
-      const ticket = req.body;
-      ticket.id = Date.now(); // Assign a unique id
-      await ticketsCollection.insertOne(ticket);
-      console.log(`Created ticket with id ${ticket.id}`);
-      res.send(ticket);
-    } catch (err) {
-      console.error('Failed to create ticket in MongoDB:', err);
-      res.status(500).send('Failed to create ticket');
-    }
-  });
-
-  // Endpoint to update a ticket
-  app.put('/rest/ticket/:id', async (req, res) => {
-    try {
-      const id = parseInt(req.params.id);
-      const ticket = req.body;
-      await ticketsCollection.updateOne({ id }, { $set: ticket });
-
-      res.send(`Updated ticket with id ${id}`);
-    } catch (err) {
-      console.error('Failed to update ticket in MongoDB:', err);
-      res.status(500).send('Failed to update ticket');
-    }
-  });
-
-  // Endpoint to delete a ticket
-  app.delete('/rest/ticket/:id', async (req, res) => {
-    try {
-      const id = parseInt(req.params.id);
-      await ticketsCollection.deleteOne({ id });
-
-      res.send(`Deleted ticket with id ${id}`);
-    } catch (err) {
-      console.error('Failed to delete ticket in MongoDB:', err);
-      res.status(500).send('Failed to delete ticket');
-    }
-  });
-
-  // Start the server
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+// Endpoint to get all tickets
+app.get('/rest/list', async (req, res) => {
+  const tickets = client.db('Phase-ll').collection('CMPS415');
+  const result = await tickets.find().toArray();
+  res.send(result);
 });
-``
+
+// Endpoint to get a single ticket by id
+app.get('/rest/ticket/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const tickets = client.db('Phase-ll').collection('CMPS415');
+  const ticket = await tickets.findOne({ id: id });
+
+  if (!ticket) {
+    res.status(404).send('Ticket not found');
+  } else {
+    res.send(ticket);
+  }
+});
+
+// Endpoint to create a new ticket
+app.post('/rest/ticket', async (req, res) => {
+  const ticket = req.body;
+  ticket.id = Date.now(); // Assign a unique id
+  const tickets = client.db('Phase-ll').collection('CMPS415');
+  await tickets.insertOne(ticket);
+  console.log(`Created ticket with id ${ticket.id}`);
+  res.send(ticket);
+});
+
+// Endpoint to update a ticket by id
+app.put('/rest/ticket/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const update = req.body;
+  const tickets = client.db('Phase-ll').collection('CMPS415');
+  const result = await tickets.updateOne({ id: id }, { $set: update });
+
+  if (result.modifiedCount === 0) {
+    res.status(404).send('Ticket not found');
+  } else {
+    console.log(`Updated ticket with id ${id}`);
+    res.send('Ticket updated successfully');
+  }
+});
+
+// Endpoint to delete a ticket by id
+app.delete('/rest/ticket/:id', async (req, res) => {
+  const id = parseInt(req.params.id);
+  const tickets = client.db('Phase-ll').collection('CMPS415');
+  const result = await tickets.deleteOne({ id: id });
+
+  if (result.deletedCount === 0) {
+    res.status(404).send('Ticket not found');
+  } else {
+    console.log(`Deleted ticket with id ${id}`);
+    res.send('Ticket deleted successfully');
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+
