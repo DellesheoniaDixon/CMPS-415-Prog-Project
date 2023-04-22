2c59a18ac6239d8aae56d25920a52f1cc6d68819
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-21 22:24:06 -0500

    Update phase2.js

--- phase2.js
+++ phase2.js
@@ -63,24 +63,25 @@ app.get('/rest/ticket/:id', async (req, res) => {
 });
 
 
+
 // // Define a route for creating a new ticket
 // app.post('/rest/ticket', async (req, res) => {
 //   // Confirm that req.body is an object
 //   if (typeof req.body === 'object' && !Array.isArray(req.body)) {
-//     const ticket = req.body;
-//     ticket.id = Date.now(); // Assign a unique id
-//     ticket.created_at = new Date(); // Set created_at field
-//     ticket.updated_at = new Date(); // Set updated_at field
-//     ticket.type = req.body.type; // Set type field
-//     ticket.subject = req.body.subject; // Set subject field
-//     ticket.description = req.body.description; // Set description field
-//     ticket.priority = req.body.priority; // Set priority field
-//     ticket.status = req.body.status; // Set status field
-//     ticket.recipient = req.body.recipient; // Set recipient field
-//     ticket.submitter = req.body.submitter; // Set submitter field
-//     ticket.assignee_id = req.body.assignee_id; // Set assignee_id field
-//     ticket.followers_ids = req.body.followers_ids; // Set followers_ids field
-
+//     const ticket = {
+//       id: Date.now(), // Assign a unique id
+//       created_at: new Date(), // Set created_at field
+//       updated_at: new Date(), // Set updated_at field
+//       type: req.body.type, // Set type field
+//       subject: req.body.subject, // Set subject field
+//       description: req.body.description, // Set description field
+//       priority: req.body.priority, // Set priority field
+//       status: req.body.status, // Set status field
+//       recipient: req.body.recipient, // Set recipient field
+//       submitter: req.body.submitter, // Set submitter field
+//       assignee_id: req.body.assignee_id, // Set assignee_id field
+//       followers_ids: req.body.followers_ids, // Set followers_ids field
+//     };
 
 //     // Confirm that ticket is an object
 //     if (typeof ticket === 'object' && !Array.isArray(ticket)) {
@@ -104,6 +105,7 @@ app.get('/rest/ticket/:id', async (req, res) => {
 //     res.status(400).send('Request body is not a valid object');
 //   }
 // });
+
 
 // Define a route for creating a new ticket
 app.post('/rest/ticket', async (req, res) => {
@@ -130,11 +132,8 @@ app.post('/rest/ticket', async (req, res) => {
       await tickets.insertOne(ticket);
       console.log(`Created ticket with id ${ticket.id}`);
 
-      // Extract the required fields from the created ticket
-      const { id, created_at, updated_at, type, subject, description, priority, status, recipient, submitter, assignee_id, followers_ids } = ticket;
-
-      // Create a response object with the required fields
-      const response = { id, created_at, updated_at, type, subject, description, priority, status, recipient, submitter, assignee_id, followers_ids };
+      // Create a response object with all the fields from the ticket object
+      const response = { ...ticket };
 
       res.send(response);
     } else {
