3c2d2cadf1f92b415bb25984ac8c3acc78276464
Dellesheonia Dixon <dellesheoniadixon@yahoo.com>
2023-04-21 20:30:03 -0500

    Update phase2.js

--- phase2.js
+++ phase2.js
@@ -1,137 +1,104 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
+const MongoClient = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
+const dbName = 'mydb';
 
-// Connection URL and database name
-const uri = 'mongodb+srv://dellesheoniadixon2:k9UyT796OyDNGNlB@ddixonmdb.05vfedo.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri, { useNewUrlParser: true });
+app.listen(port);
+console.log('Server started at port:' + port);
 
-// Connect to MongoDB
-async function connect() {
-  try {
-    await client.connect();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// ---------------------
+// Routes for the app
+// ---------------------
+
+app.get('/', function (req, res) {
+  const myquery = req.query;
+  var outstring = 'Started and listening... ';
+  res.send(outstring);
+});
+
+// Show the form for POSTing input
+app.get('/form', function (req, res) {
+  res.setHeader('Content-Type', 'text/html');
+  fs.readFile('./post.html', 'utf8', (err, contents) => {
+    if (err) {
+      console.log('Form file Read Error', err);
+      res.write("<p>Form file Read Error");
+    } else {
+      console.log('Form loaded\n');
+      res.write(contents + "<br>");
+    }
+    res.end();
+  });
+});
+
+// List all tickets (which are stored in MongoDB)
+app.get('/rest/list', function (req, res) {
+  MongoClient.connect(url, function (err, client) {
+    if (err) throw err;
     console.log('Connected to MongoDB');
-  } catch (err) {
-    console.error(err);
-  }
-}
-connect();
-
-// Middleware to parse request body as JSON
-app.use(bodyParser.json());
-
-// Middleware to parse form data
-app.use(bodyParser.urlencoded({ extended: true }));
-
-// Endpoint to get all tickets
-app.get('/rest/list', async (req, res) => {
-  const tickets = client.db('Phase-ll').collection('CMPS415');
-  const result = await tickets.find().toArray();
-  res.send(result);
+    const db = client.db(dbName);
+    const ticketsCollection = db.collection('tickets');
+    ticketsCollection.find({}).toArray(function (err, tickets) {
+      if (err) throw err;
+      console.log('Tickets loaded\n');
+      res.write(JSON.stringify(tickets) + "<br>");
+      res.end();
+      client.close();
+    });
+  });
 });
 
-// Define a route for retrieving a ticket by id
-app.get('/rest/ticket/:id', async (req, res) => {
-  const ticketId = parseFloat(req.params.id); // Parse id as double
-  const tickets = client.db('Phase-ll').collection('CMPS415');
+// Search for a specific ticket (id) in MongoDB
+app.get('/rest/ticket/:id', function (req, res) {
+  const inputId = Number(req.params.id);
+  console.log("Looking for: " + inputId);
 
-  // Use findOne() method to find a document by id
-  const ticket = await tickets.findOne({ id: ticketId });
-
-  if (ticket) {
-    console.log(`Retrieved ticket with id ${ticketId}`);
-    res.send(ticket);
-  } else {
-    console.log(`Ticket with id ${ticketId} not found`);
-    res.status(404).send(`Ticket with id ${ticketId} not found`);
-  }
+  MongoClient.connect(url, function (err, client) {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
+    const ticketsCollection = db.collection('tickets');
+    ticketsCollection.findOne({ id: inputId }, function (err, ticket) {
+      if (err) throw err;
+      if (!ticket) {
+        res.status(404).send("Ticket does not exist!");
+      } else {
+        console.log("Ticket exists!")
+        res.json(ticket);
+      }
+      client.close();
+    });
+  });
 });
 
-// Define a route for creating a new ticket
-app.post('/rest/ticket', async (req, res) => {
-  // Confirm that req.body is an object
-  if (typeof req.body === 'object' && !Array.isArray(req.body)) {
-    const ticket = req.body;
-    ticket.id = Date.now(); // Assign a unique id
+// A POST request to insert a new record in MongoDB
+app.post('/rest/maketicket', function (req, res) {
+  const newTicket = req.body;
+  // Report to console what was received (for debugging)
+  console.log(newTicket);
 
-    // Confirm that ticket is an object
-    if (typeof ticket === 'object' && !Array.isArray(ticket)) {
-      const tickets = client.db('Phase-ll').collection('CMPS415');
-      await tickets.insertOne(ticket);
-      console.log(`Created ticket with id ${ticket.id}`);
-      res.send(ticket);
-    } else {
-      console.log('Ticket data is not a valid object');
-      res.status(400).send('Ticket data is not a valid object');
-    }
-  } else {
-    console.log('Request body is not a valid object');
-    res.status(400).send('Request body is not a valid object');
-  }
+  MongoClient.connect(url, function (err, client) {
+    if (err) throw err;
+    console.log('Connected to MongoDB');
+    const db = client.db(dbName);
+    const ticketsCollection = db.collection('tickets');
+    ticketsCollection.insertOne(newTicket, function (err, result) {
+      if (err) {
+        return res.status(500).json({
+          error: "Error writing to MongoDB!"
+        });
+      } else {
+        console.log("Ticket added!")
+        res.status(201).json(newTicket);
+      }
+      client.close();
+    });
+  });
 });
 
-// Define a route for handling form submission
-app.post('/post.html', async (req, res) => {
-  // Access form data from req.body
-  const formData = req.body;
-  // Process form data and create a new ticket
-  // ... (your code here)
-  // Redirect or send response as needed
-  // ... (your code here)
-});
-
-//Define a route to update a ticket
-app.put('/rest/ticket/:id', async (req, res) => {
-  try {
-    const ticketId = parseInt(req.params.id);
-    const updatedTicket = req.body;
-    delete updated
-    const tickets = client.db('Phase-ll').collection('CMPS415');
-    // Use findOneAndUpdate() method to update the ticket
-    const result = await tickets.findOneAndUpdate(
-      { id: ticketId }, // Filter for the ticket to be updated
-      { $set: updatedTicket }, // Update the ticket with the new data
-      { returnOriginal: false } // Return the updated ticket
-    );
-
-    if (result.value) {
-      console.log(`Updated ticket with id ${ticketId}`);
-      res.send(result.value);
-    } else {
-      console.log(`Ticket with id ${ticketId} not found`);
-      res.status(404).send(`Ticket with id ${ticketId} not found`);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Define a route for deleting a ticket
-app.delete('/rest/ticket/:id', async (req, res) => {
-  try {
-    const ticketId = parseInt(req.params.id);
-    const tickets = client.db('Phase-ll').collection('CMPS415');
-    // Use deleteOne() method to delete the ticket by id
-    const result = await tickets.deleteOne({ id: ticketId });
-
-    if (result.deletedCount > 0) {
-      console.log(`Deleted ticket with id ${ticketId}`);
-      res.send(`Deleted ticket with id ${ticketId}`);
-    } else {
-      console.log(`Ticket with id ${ticketId} not found`);
-      res.status(404).send(`Ticket with id ${ticketId} not found`);
-    }
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Internal Server Error');
-  }
-});
-
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
